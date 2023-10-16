8ece930718c1b46cca6c9497276b5e96f909c573
Alex Kornilov <alex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.com>
2023-10-16 15:39:05 +0300

    created the storyboard and add media files

---
+++ 10.My adventure line/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  10.My adventure line
+//
+//  Created by Alex  on 16.10.2023.
+//
+
+import Foundation
---
+++ 10.My adventure line/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  10.My adventure line
+//
+//  Created by Alex  on 16.10.2023.
+//
+
+import Foundation
