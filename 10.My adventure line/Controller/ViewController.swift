9176f29237ab85310c46cc270141becbc8a98d04
Alex Kornilov <alex.kornilovv2001@gmail.com>
2023-11-11 19:13:08 +0300

    add stories way & some changes in code

--- 10.My adventure line/Controller/ViewController.swift
+++ 10.My adventure line/Controller/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var storyLabel: UILabel!
+	@IBOutlet weak var choice1Button: UIButton!
+	@IBOutlet weak var choice2Button: UIButton!
+	
+	var storyBrain = StoryBrain()
+	
+	let story0 = "You see a fork in the road"
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		updateUI()
 	}
 
-
+	@IBAction func choiceMade(_ sender: UIButton) {
+		
+		
+		updateUI()
+	}
+	
+	func updateUI() {
+		storyLabel.text = story0
+		choice1Button.setTitle("Take a left", for: .normal)
+		choice2Button.setTitle("Take a right", for: .normal)
+	}
+	
 }
 
